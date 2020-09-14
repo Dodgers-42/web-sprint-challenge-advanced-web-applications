ed80aa28e6eee2cdcfe170e7e45c4fde4731cc57
Dodger-42 <kevin.bing.barber@gmail.com>
2020-09-14 16:31:04 -0700

    working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
+
+  const [from, setForm] = useState({
+    username: '',
+    password: '',
+  });
+
+  const onChange = event => {}
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
